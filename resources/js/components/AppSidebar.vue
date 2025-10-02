36f7c4b63fd8f796941f35b9c5342b48c3464391
Antony Checo <80710861+acheco@users.noreply.github.com>
2025-10-01 21:49:53 -0400

    Changing AppLogo, adding sidebar menu options

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
 import {
@@ -14,7 +13,14 @@ import {
 import { dashboard } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import {
+    ArrowUpDown,
+    ChartPie,
+    LayoutGrid,
+    Receipt,
+    Settings,
+    Vault,
+} from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
@@ -23,20 +29,45 @@ const mainNavItems: NavItem[] = [
         href: dashboard(),
         icon: LayoutGrid,
     },
+    {
+        title: 'Transactions',
+        href: '/transactions',
+        icon: ArrowUpDown,
+    },
+    {
+        title: 'budgets',
+        href: '/budgets',
+        icon: ChartPie,
+    },
+    {
+        title: 'Pots',
+        href: '/pots',
+        icon: Vault,
+    },
+    {
+        title: 'Recurring Bills',
+        href: '/recurring-bills',
+        icon: Receipt,
+    },
+    {
+        title: 'App Settings',
+        href: '/app-settings',
+        icon: Settings,
+    },
 ];
 
-const footerNavItems: NavItem[] = [
-    {
-        title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
-    },
-];
+// const footerNavItems: NavItem[] = [
+//     {
+//         title: 'Github Repo',
+//         href: 'https://github.com/laravel/vue-starter-kit',
+//         icon: Folder,
+//     },
+//     {
+//         title: 'Documentation',
+//         href: 'https://laravel.com/docs/starter-kits#vue',
+//         icon: BookOpen,
+//     },
+// ];
 </script>
 
 <template>
@@ -58,7 +89,7 @@ const footerNavItems: NavItem[] = [
         </SidebarContent>
 
         <SidebarFooter>
-            <NavFooter :items="footerNavItems" />
+            <!--            <NavFooter :items="footerNavItems" />-->
             <NavUser />
         </SidebarFooter>
     </Sidebar>
--- resources/js/layouts/auth/AuthSplitLayout.vue
+++ resources/js/layouts/auth/AuthSplitLayout.vue
@@ -14,8 +14,13 @@ defineProps<{
 </script>
 
 <template>
+    <header
+        class="rounded-b-xl bg-grey-900 py-4 text-center text-2xl font-bold text-white lg:hidden"
+    >
+        Finance
+    </header>
     <div
-        class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0"
+        class="relative grid h-dvh flex-col items-center justify-center sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0"
     >
         <div
             class="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r"
@@ -25,21 +30,32 @@ defineProps<{
                 :href="home()"
                 class="relative z-20 flex items-center text-lg font-medium"
             >
-                <AppLogoIcon class="mr-2 size-8 fill-current text-white" />
                 {{ name }}
             </Link>
+            <div class="relative z-20 flex items-center mt-auto">
+                <AppLogoIcon class="mr-2 size-8 fill-current text-white" />
+
+            </div>
             <div v-if="quote" class="relative z-20 mt-auto">
-                <blockquote class="space-y-2">
-                    <p class="text-lg">&ldquo;{{ quote.message }}&rdquo;</p>
+                <blockquote class="space-y-1">
+                    <p class="text-2xl font-bold">Keep track of your money</p>
+                    <p class="text-2xl font-bold">and save for your future</p>
                     <footer class="text-sm text-neutral-300">
-                        {{ quote.author }}
+                        <p>
+                            Personal finance app puts you in control of your
+                            spending. track
+                        </p>
+                        <p>
+                            transactions, set budgets and add to savings pots
+                            easily.
+                        </p>
                     </footer>
                 </blockquote>
             </div>
         </div>
         <div class="lg:p-8">
             <div
-                class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]"
+                class="mx-auto flex w-full flex-col justify-center space-y-6 rounded-xl bg-white p-4 sm:w-[350px] md:w-[450px] md:p-10"
             >
                 <div class="flex flex-col space-y-2 text-center">
                     <h1 class="text-xl font-medium tracking-tight" v-if="title">
